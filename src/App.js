e5000106c2aee517836c75d09782e5d01d4d3b2f
Jason Cordova <jasoncordova.edu@gmail.com>
2025-05-26 20:25:36 -0400

    landing fix

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
       <div className="landing">
 
-        <Message type="sender" message="Hey, this is Jason&nbsp;" emoji="wave"/>
+        <Message type="sender" message="Hey, this is Jason" emoji="wave"/>
         <Message type="sender" message="I'm a Software Engineer located in the New York Metro area"/>
 
       </div>
--- src/comp/Message/index.js
+++ src/comp/Message/index.js
@@ -1,26 +1,16 @@
-import { useEffect, useRef } from 'react';
 import wave from '../../assets/wave.webp';
 import './index.css';
 
 const Message = (props) => {
 
-    const messageElement = useRef(null);
-
-    useEffect(() => {
-
-        messageElement.current.innerHTML = `${props.message}`;
-
-        if (props.emoji){
-            messageElement.current.innerHTML += `<img alt="Wave emoji" class="emoji" src=${wave}/>`
-        }
-
-    }, [props]);
-
     return (
 
         <div className={`message clipped ${props.type === "sender" ? "sender" : ""}`}>
 
-            <div className="message-content" ref={messageElement}></div>
+            <div className="message-content">
+                <span>{props.message}</span>
+                {props.emoji != null ? <img class="emoji" src={wave} alt="Hand waving"></img> : ""}
+            </div>
             <svg className="message-edge" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.6 18.971"><path d="M0 12.259a24.319 24.319 0 0 0 8.426 4.781A53.363 53.363 0 0 0 20.6 18.971s-7.7-5.226-8.957-10.48a54.431 54.431 0 0 1-.937-8.49s-3.274 2.708-5.951 5.11S0 9.611 0 9.611v2.649Z"/></svg>
 
         </div>
