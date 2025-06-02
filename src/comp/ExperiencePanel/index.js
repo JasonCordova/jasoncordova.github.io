239988f8d8c0a0674446e0f85d96815b17c0f630
Jason Cordova <jasoncordova.edu@gmail.com>
2025-06-01 20:06:17 -0400

    Added experience

--- src/comp/ExperiencePanel/index.js
+++ src/comp/ExperiencePanel/index.js
@@ -1,15 +1,31 @@
 import './index.css';
+import { useState } from 'react';
 
 const ExperiencePanel = () => {
 
-    return (
-        <div class="experience">
+    const [tab, setTab] = useState("work");
 
-            <div className="panel-title bg-clip">
-                <span>JC</span>
+    return (
+        <>
+        <div className="experience">
+
+            <div className="dot-bg"></div>
+            <div className="panel-title">Experience</div>
+
+            <div className="tab-list">
+
+                <div className="experience-tab">
+
+                    <div className={`tab-cell${tab === "education" ? " active" : ""}`}></div>
+                    <div className={`tab-option${tab === "work" ? " active" : ""}`} onClick={() => {setTab("work");}}>Work</div>
+                    <div className={`tab-option${tab === "education" ? " active" : ""}`} onClick={() => {setTab("education");}}>Education</div>
+
+                </div>
+
             </div>
 
         </div>
+        </>
     );
 
 }
